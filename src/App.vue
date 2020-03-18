9399bf6d8dda385bf93566c6216e168f6c0e800d
luojie <1015779635@qq.com>
2020-03-18 17:31:46 +0800

    基本实现全球疫情现存人数可视化

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+	
+html{
+    height:100%;
+    width:100%;
+}	
+	
+body{
+	height:100%;
+	width:100%;
+}	
+	
+#app {
+  height: 100%;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-image: url(assets/bg.png);
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import echarts from 'echarts'
+Vue.prototype.$echarts = echarts
+
+import '../node_modules/echarts/map/js/world.js' // 引入世界地图
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
